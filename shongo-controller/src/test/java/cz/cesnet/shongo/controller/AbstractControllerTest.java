812581e88c74d1eed821d23c964cfaa9d32cb98b
Filip Karnis <karnisfilip@gmail.com>
2023-08-02 19:33:03 +0200

    add test configuration

--- shongo-controller/src/test/java/cz/cesnet/shongo/controller/AbstractControllerTest.java
+++ shongo-controller/src/test/java/cz/cesnet/shongo/controller/AbstractControllerTest.java
@@ -22,6 +22,7 @@ import org.joda.time.Interval;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -288,6 +289,7 @@ public abstract class AbstractControllerTest extends AbstractDatabaseTest
             configureSystemProperties();
 
             // Create controller
+            final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
             controller = cz.cesnet.shongo.controller.Controller.create(new Controller(null)
             {
                 @Override
@@ -323,7 +325,7 @@ public abstract class AbstractControllerTest extends AbstractDatabaseTest
                         super.stop();
                     }
                 }
-            });
+            };
             controller.setDomain("cz.cesnet", "CESNET, z.s.p.o.");
             controller.setEntityManagerFactory(getEntityManagerFactory());
 
